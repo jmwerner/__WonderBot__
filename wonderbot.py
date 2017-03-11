b2883f101e02b3513ea4eb60ac582824e876936e
Jeremy Werner <jmwerner1123@me.com>
2017-03-11 14:28:33 -0800

    Add batch job for comments and posts because streams inevitably crash

--- wonderbot.py
+++ wonderbot.py
@@ -5,7 +5,6 @@ import sys
 import os
 import json
 import praw
-from pprint import pprint
 
 LOGIN_FILE = 'login_info.json'
 
@@ -13,7 +12,7 @@ LOGIN_FILE = 'login_info.json'
 class WonderBot:
     '''This is the reddit bot class for handling all interaction with the web'''
     def __init__(self, login_file, subreddit_name):
-        self.new_post_limit = 10
+        self.new_item_limit = 30
         self.bot_name = 'Tony Wonder'
         self.bot_account = '__WonderBot__'
         self.keyword = 'wonder'
@@ -76,7 +75,17 @@ class WonderBot:
         Returns:
             Generator of new posts for subreddit.
         '''
-        newest_posts = self.subreddit.new(limit=self.new_post_limit)
+        newest_posts = self.subreddit.new(limit=self.new_item_limit)
+        return newest_posts
+
+    def get_new_comments(self):
+        '''Returns new posts for specified subreddit.
+        Args:
+            None
+        Returns:
+            Generator of new posts for subreddit.
+        '''
+        newest_posts = self.subreddit.comments(limit=self.new_item_limit)
         return newest_posts
 
     def get_comments_from_post(self, post_id):
@@ -97,13 +106,18 @@ class WonderBot:
         Returns:
             logical: True if self.keyword is within the text.
         '''
-        punctuation = ('?',':','!','.',',',';')
+        punctuation = ('?', ':', '!', '.', ',', ';')
         stripped_string = ''.join(c for c in text if c not in punctuation)
         words_list = stripped_string.lower().split()
         return self.keyword in words_list
 
     def process_comment(self, comment):
-        ''''''
+        '''Processes and replies to a given comment if appropriate.
+        Args:
+            comment (object): Praw comment object.
+        Returns:
+            None
+        '''
         if comment.author != self.bot_account:
             if self.submission_needs_reply(comment.body):
                 if not self.id_in_log(comment.id, self.comments_log_name):
@@ -112,7 +126,12 @@ class WonderBot:
                     comment.reply(self.reply_text)
 
     def process_submission(self, submission):
-        ''''''
+        '''Processes and replies to a given submission if appropriate.
+        Args:
+            submission (object): Praw submission object.
+        Returns:
+            None
+        '''
         if submission.author != self.bot_account:
             all_text = submission.selftext + ' ' + submission.title
             if self.submission_needs_reply(all_text):
@@ -122,13 +141,23 @@ class WonderBot:
                     submission.reply(self.reply_text)
 
     def start_comment_stream(self):
-        ''''''
+        '''Depreciated'''
         for post in reddit_bot.subreddit.stream.comments():
             self.process_comment(post)
 
     def start_submission_stream(self):
-        ''''''
+        '''Depreciated'''
         for submission in reddit_bot.subreddit.stream.submissions():
+            self.process_submission(submission)
+
+    def start_comment_batch(self):
+        ''''''
+        for post in self.get_new_comments():
+            self.process_comment(post)
+
+    def start_submission_batch(self):
+        ''''''
+        for submission in self.get_new_posts():
             self.process_submission(submission)
 
     def write_to_log(self, id, log_file_name):
@@ -156,9 +185,9 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:
         reddit_bot = WonderBot(login_file=LOGIN_FILE, subreddit_name='wondertest')
         if str(sys.argv[1]) == 'comments':
-            reddit_bot.start_comment_stream()
+            reddit_bot.start_comment_batch()
         elif str(sys.argv[1]) == 'submissions':
-            reddit_bot.start_submission_stream()
+            reddit_bot.start_submission_batch()
         else:
             raise ValueError('Argument ' + str(sys.argv[1]) + ' not recognized')
     else:
