e6a5f8e4d8228ba12171136a60528bed37611721
Jeremy Werner <jmwerner1123@me.com>
2017-03-11 21:31:54 -0800

    Remove nohup from job launch script

--- launch_bot.sh
+++ launch_bot.sh
@@ -2,9 +2,9 @@
 
 mkdir -p logs
 
-nohup python wonderbot.py comments > logs/comments_nohup.out 2> logs/comments_nohup.err &
+python wonderbot.py comments > logs/comments_nohup.out
 
 sleep 30
 
-nohup python wonderbot.py submissions > logs/submissions_nohup.out 2> logs/submissions_nohup.err &
+python wonderbot.py submissions > logs/submissions_nohup.out
 
